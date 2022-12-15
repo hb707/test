d2b2ec50518f884739d39b710e4f0392bfa11939
hanbin <hanbin@opusm.io>
2022-12-15 22:01:49 +0900

    apitest

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const app = express();
+
+app.use(express.static("./public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("hello world! crypto apis testing...");
+});
+
+app.post("/test", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+
+app.listen(4000, () => {
+  console.log("back server onload");
+});
